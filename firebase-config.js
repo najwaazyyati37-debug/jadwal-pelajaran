ccfd370bcbaa7f79f74b70ed8e04aa2e9de478a3
najwarpl <najwaazyyati37@gmail.com>
2025-11-27 13:40:33 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Konfigurasi Firebase milik kamu
+const firebaseConfig = {
+  apiKey: "AIzaSyDXPdmLeBWh7NN46HQScDEW2731jAA5T7k",
+  authDomain: "aplikasi-jadwal-dan-tugas.firebaseapp.com",
+  projectId: "aplikasi-jadwal-dan-tugas",
+  storageBucket: "aplikasi-jadwal-dan-tugas.firebasestorage.app",
+  messagingSenderId: "835649858878",
+  appId: "1:835649858878:web:6d1fab4136744e0bbbd06f",
+  measurementId: "G-95VV94NB3L"
+};
